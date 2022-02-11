005ea97afc8103a20cc4bc19402aabe52c3aec7e
MIKKE-WANGO <71769643+MIKKE-WANGO@users.noreply.github.com>
2022-02-11 08:32:15 +0300

    changing url route for react template to use re_path

--- notes_django/urls.py
+++ notes_django/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 
 from re import template
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    
 ]
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
